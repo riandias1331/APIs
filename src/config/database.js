6ba83f3ee88e43c8499c2c6d8cc5f87c8e3543a6
riandias1331 <pereirarian@gmail.com>
2025-03-30 14:37:24 -0300

    refactor: simplifica conexão com MongoDB removendo eventos Express

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectDB = (app, port) => {
+const connectDB = (app) => {
     mongoose.connect(process.env.DATABASE_URL)
         .then(() => {
             console.log('Connected to database')
