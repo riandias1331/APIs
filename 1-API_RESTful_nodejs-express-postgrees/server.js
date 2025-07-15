da70af0f927204c97dedfe6b01f6a2cc86b6a535
Rian Dias <pereiradiasrian.@gmail.com>
2025-07-15 11:28:10 -0300

    feat(api): start new api

--- 1-API_RESTful_nodejs-express-postgrees/server.js
+++ 1-API_RESTful_nodejs-express-postgrees/server.js
@@ -1,48 +1,5 @@
-// const mongoose = require('mongoose')
-// mongoose.connect(process.env.CONNECTION)
-//     .then(() => {
-//         console.log('Connected')
-//         app.emit('DataBase')
-//     })
-//     .catch((e) => console.log(e))
-
-
-
-
-require('dotenv').config();
-const express = require('express');
-const { Pool } = require('pg');
-
-const app = express();
-const port = process.env.PORT || 3333;
-
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Conexão com o PostgreSQL
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-});
-
-// Verificar conexão
-pool.connect()
-    .then(() => {
-        console.log('Connected to PostgreSQL');
-        app.emit('DataBase');
-    })
-    .catch((err) => console.error('Connection error', err.stack));
-
-// Rotas
-const userRoutes = require('./route');
-app.use('/users', userRoutes);
-
-// Inicializar o servidor após a conexão com o banco
-app.on('DataBase', () => {
-    app.listen(port, () => {
-        console.log(`Server is running on port: ${port}`);
-        console.log(`http://localhost:${port}/users`);
-    });
-});
-
-module.exports = pool; // Exportar o pool para usar em outros arquivos
+import dotenv from 'dotenv'
+dotenv.config()
+import express from "express"
+const app = express()
+const port = process.env.PORT || 5001
