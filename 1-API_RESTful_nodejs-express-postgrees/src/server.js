c6e91c3a520375a470f14735b0f38c35c3eadb67
ian Dias <pereiradiasrian@gmail.com>
2025-08-03 17:10:37 -0300

    feat: errorhandler

--- 1-API_RESTful_nodejs-express-postgrees/src/server.js
+++ 1-API_RESTful_nodejs-express-postgrees/src/server.js
@@ -13,7 +13,11 @@ const port = process.env.PORT || 5001
 // miidlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 // Routes
 app.use(userRoutes)
